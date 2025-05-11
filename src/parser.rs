96c1fb26ae458108d42da7a33978167e38db6726
Destroy <emptycsgo@mail.ru>
2025-05-11 08:00:49 +0300

    Regex has been fixed

--- src/parser.rs
+++ src/parser.rs
@@ -4,15 +4,7 @@ use regex::Regex;
 pub fn is_tiktok_url(message: &str) -> bool {
     lazy_static! {
         static ref RE: Regex = Regex::new(
-            r"(?ix)^https?://
-            (?:www\.|m\.|vm\.)?      # Subdomain
-            tiktok\.com/
-            (?:                       # Path options:
-                @[\w-]+/video/\d+     # Standard format with video
-                | t/\d+               # Format with /t/
-                | \w+                 # Short ID for VM
-            )
-            /?$                       # Optional finishing slash"
+            r"(?ix)^(https?://)?\w{2,3}\.tiktok\.com/.+"
         ).unwrap();
     }
     RE.is_match(message)
